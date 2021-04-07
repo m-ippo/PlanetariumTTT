733203d10a7faa24266fa4ac9572b82627924dc3
ThatCommand <g.vianello002@studenti.unibs.it>
2021-04-07 16:43:34 +0200

    Update GestioneInput.java

--- src/planetarium/input/GestioneInput.java
+++ src/planetarium/input/GestioneInput.java
@@ -4,19 +4,24 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GestioneInput {
-    private static final Scanner input = new Scanner(System.in);
-    public static double leggiDouble () {
-        double valoreLetto;
-        try {
-            valoreLetto = input.nextDouble();
 
-        } catch (InputMismatchException ime){
-            System.out.println("Inserire un valore corretto");
-            valoreLetto = 0;
+    private static final Scanner input = new Scanner(System.in);
+
+    public static double leggiDouble() {
+        double valoreLetto = 0.0;
+        boolean corretto = false;
+
+        while (!corretto) {
+            try {
+                valoreLetto = input.nextDouble();
+                corretto = true;
+            } catch (InputMismatchException ime) {
+                input.nextLine();
+                System.out.println("Inserire un valore corretto");
+            }
         }
 
         return valoreLetto;
     }
-
 
 }

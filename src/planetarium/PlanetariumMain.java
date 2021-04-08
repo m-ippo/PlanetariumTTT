c8cda03c477c0f7587dd34df13a02b25e5499a3e
FadaNicholas <n.fada001@studenti.unibs.it>
2021-04-08 18:25:40 +0200

    Rimosso package "Operazioni" (non utilizzato)

--- src/planetarium/PlanetariumMain.java
+++ src/planetarium/PlanetariumMain.java
@@ -32,29 +32,6 @@ public class PlanetariumMain {
         
         Menu nuovoMenu = new Menu();
         nuovoMenu.stampaMenu();
-        //commento di prova guygf
 
-        /*
-        Luna l = new Luna("luna 1", 5677984.54, new Posizione(3,6));
-        Luna l1 = new Luna( "luna 2", 5677984.54, new Posizione(3,6));
-        Luna l2 = new Luna("luna 3", 5677984.54, new Posizione(3,6));
-
-        Pianeta p = new Pianeta("terra 1", 657348, new Posizione(3,6));
-        Pianeta p1 = new Pianeta( "terra 2", 657348, new Posizione(3,6));
-        Pianeta p2 = new Pianeta( "terra 3", 657348, new Posizione(3,6));
-
-        Stella s = Stella.generateIstance("sole", 5679846, new Posizione(3,6));
-
-        s.aggiungiCorpoCeleste(p);
-
-        p.aggiungiCorpoCeleste(l);
-        p.aggiungiCorpoCeleste(l1);
-        p.aggiungiCorpoCeleste(l2);
-
-        p.stampaOrbita();
-
-        l.distruggi();
-
-        p.stampaOrbita();*/
     }
 }
